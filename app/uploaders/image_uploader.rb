8158877a41356f5c8cc9768e7f4b1f83537834ac
burnssa@gmail.com <burnssa@gmail.com>
2014-08-03 09:42:50 -0700

    added uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fill => [300, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   #Create different versions of your uploaded files:
-  process :resize_to_fill => [300, 300]
+  #process :resize_to_fill => [300, 300]
 
   version :thumb do
     process :resize_to_fill => [50, 50]
