48c6f3126c9dc4a406c8928b5e9f0de2a6281788
burnssa@gmail.com <burnssa@gmail.com>
2014-08-05 22:30:14 -0700

    refactored policies

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,7 +4,7 @@ class PostPolicy < ApplicationPolicy
 	end
 
 	def destroy?
-		user.present? && (record.user == user || user.role?(:admin) || user.role?(:moderator))
+		user.present? && can_moderate?(user, record)
 	end
 
 end
